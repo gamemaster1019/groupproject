81b9179a45ae96f5f8f2d48bd75fa84ec8a47887
antonio anthony <antonioanthony19@gmail.com>
2021-04-16 00:51:41 -0400

    intial commit

---
+++ src/main/java/com/company/Summative2AnthonyAntonio/Summative2AnthonyAntonioApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative2AnthonyAntonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative2AnthonyAntonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative2AnthonyAntonioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Summative2AnthonyAntonio/Summative2AnthonyAntonioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative2AnthonyAntonio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative2AnthonyAntonioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
